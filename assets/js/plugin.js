a63a556699afa94c5dcdd887db674d432b4e5ee5
eng-heba-yassin <eng.heba_yassin@yahoo.com>
2020-08-29 06:03:06 +0200

    first commit

---
+++ assets/js/plugin.js
@@ -0,0 +1,55 @@
+﻿// Blog Carousel
+$('.blog-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    rtl: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: true
+        },
+        1000: {
+            items: 3,
+            nav: true,
+        }
+    }
+});
+
+// Blog Carousel
+$('.b-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    rtl: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: true
+        },
+        1000: {
+            items: 3,
+            nav: true,
+        }
+    }
+})
+
+// Map
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
